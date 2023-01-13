762095fc41156690d6266c9b4cf1b2648562dd7a
Onur Arda Karakum <onur.ardakarakum@outlook.com>
2023-01-13 10:15:52 +0300

    LanguagePage || Language page UI completed

--- lib/app/modules/language_page/language_view.dart
+++ lib/app/modules/language_page/language_view.dart
@@ -10,13 +10,162 @@ class LanguagePage extends GetView<LanguageController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xffEF4D58),
       appBar: AppBar(
-        elevation: 0,
-        toolbarHeight: 0,
-        backgroundColor: const Color(0xffEF4D58),
+          leading: Icon(Icons.arrow_back_ios, size: 3.h, color: Colors.white),
+          elevation: 0,
+          centerTitle: true,
+          title: Text(
+            "Language",
+            style: GoogleFonts.inter(
+                color: Colors.white,
+                fontSize: 3.2.h,
+                fontWeight: FontWeight.w600),
+          ),
+          backgroundColor: const Color(0xff94A3B8)),
+      body: SingleChildScrollView(
+        child: Container(
+          height: 100.h,
+          width: 100.w,
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image:
+                      AssetImage("assets/images/language/login_background.jpg"),
+                  fit: BoxFit.cover)),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 5.w),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 86.h,
+                  width: 100.w,
+                  // margin: EdgeInsets.only(top: 2.h),
+                  child: ListView.builder(
+                    padding: EdgeInsets.only(top: 3.h),
+                    itemCount: controller.languages.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        height: 9.h,
+                        width: 100.w,
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 2.w, vertical: 1.h),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(2.w),
+                            color: Colors.white,
+                            border: Border.all(
+                              width: .5.w,
+                              color: const Color(0xff94A3B8),
+                            )),
+                        child: Row(
+                          children: [
+                            Container(
+                              height: 15.h,
+                              width: 30.w,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          controller.languages[index]["flag"]),
+                                      fit: BoxFit.cover)),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(left: 4.w, top: 1.2.h),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    controller.languages[index]["name"],
+                                    style: GoogleFonts.inter(
+                                        color: Colors.black,
+                                        fontSize: 2.7.h,
+                                        fontWeight: FontWeight.w500),
+                                  ),
+                                  SizedBox(
+                                    height: .3.h,
+                                  ),
+                                  Text(
+                                    controller.languages[index]["subTitle"],
+                                    style: GoogleFonts.inter(
+                                        color: Colors.black,
+                                        fontSize: 1.7.h,
+                                        fontWeight: FontWeight.w400),
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      );
+
+                      //
+                      /* Padding(
+                        padding: EdgeInsets.only(bottom: 2.h),
+                        child: //burada obx olacak checkbox için
+                            ListTile(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(3.w)),
+                          tileColor: const Color(0xffeeeeee),
+                          onTap: () {
+                            // controller.checkBoxControl();
+                          },
+                          title: Text(
+                            controller.languages[index]["name"],
+                            style: GoogleFonts.inter(color: Colors.black),
+                          ),
+                          subtitle: Text(
+                              controller.languages[index]["subTitle"],
+                              style: GoogleFonts.inter()),
+                          /* trailing: Checkbox(
+                            onChanged: (value) {
+                              controller.languages[index]["isCheck"] == true
+                                  ? controller.languages[index]["isCheck"] =
+                                      false
+                                  : controller.languages[index]["isCheck"] =
+                                      true;
+                              controller.languages.refresh();
+                              //controller.checkBoxControl(index);
+                            },
+                            value: controller.languages[index]["isCheck"],
+                          ), */
+                          leading: Container(
+                            height: 15.h,
+                            width: 22.w,
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                                    image: AssetImage(
+                                        controller.languages[index]["flag"]),
+                                    fit: BoxFit.cover)),
+                          ),
+                        ),
+                      ); */
+                      //
+                      /* Container(
+                        height: 8.h,
+                        width: 100.w,
+                        margin: EdgeInsets.symmetric(
+                            horizontal: 2.w, vertical: 1.h),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(2.w),
+                            color: Colors.white,
+                            border: Border.all(
+                              width: .5.w,
+                              color: const Color(0xff94A3B8),
+                            )),
+                      ); */
+                    },
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-      body: Padding(
+    );
+  }
+}
+//eskisi
+        /* Padding(
         padding: EdgeInsets.symmetric(horizontal: 5.w),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
@@ -126,7 +275,8 @@ class LanguagePage extends GetView<LanguageController> {
             )
           ],
         ),
-      ),
-    );
+      ), 
+        );
   }
 }
+  }*/

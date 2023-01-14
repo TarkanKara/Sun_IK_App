5637f9b6c7d9d2b80f880a4e79417c87f3ac3804
Tarkan Kara <unnahmet23@hotmail.com>
2023-01-14 11:14:10 +0300

    develop || Updated

--- lib/app/modules/start_page/start_view.dart
+++ lib/app/modules/start_page/start_view.dart
@@ -7,21 +7,8 @@ class StartView extends GetView<StartController> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Obx(() => controller.status.value.isSuccess
-              ? Column(
-                  children: [
-                    Text(
-                      "Talep No : ${controller.myRwquestGetPending.data!.pendingRequestList![0].iDMASTER}",
-                      style: const TextStyle(fontSize: 50),
-                    )
-                  ],
-                )
-              : const Center(child: CircularProgressIndicator())),
-        ),
-      ),
+    return const SafeArea(
+      child: Scaffold(),
     );
   }
 }

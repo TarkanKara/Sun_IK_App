be81bb928957d901990f086fc99cef0b25d4529b
Onur A. Karakum <113251720+oakarakum@users.noreply.github.com>
2023-01-16 19:30:03 +0300

    myJobsPage || UI and info filled

--- lib/api/interceptors/request_interceptor.dart
+++ lib/api/interceptors/request_interceptor.dart
@@ -4,6 +4,6 @@ import 'package:get/get_connect/http/src/request/request.dart';
 
 FutureOr<Request> requestInterceptor(request) async {
   request.headers["vbtauthorization"] =
-      "TUTbQ3An8fW1qhn/IZl1Y6utIZ8KnpD3c0zvzgz6RK4YMn6A3A/DJh0f8cEQ9MnK~1742~string~638093321180821499";
+      "vs7jGaVLi4yRStDBUsv1CGJofQfvyAnGcFtxOU4DzTm/xUunctky6NuGDq6Ojwf4~1~string~638094926745843088";
   return request;
 }
--- lib/app/modules/my_jobs_page/my_jobs_view.dart
+++ lib/app/modules/my_jobs_page/my_jobs_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:sun_ik_app/utils/date_convert.dart';
 
+import '../../../utils/const.dart';
 import 'my_jobs_controller.dart';
 
 class MyJobsView extends GetView<MyJobsController> {
@@ -10,26 +13,208 @@ class MyJobsView extends GetView<MyJobsController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Obx(() => controller.status.value.isSuccess
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Tarih : ${DateTimeConverTo.compareToDateTime(controller.myJobs.data!.pendingJobsList![0].wORKCREATIONDATE.toString())}",
-                  ),
-                  Text(
-                    "İş No: ${controller.myJobs.data!.pendingJobsList![0].iDWORK}",
-                  ),
-                  Text(
-                    "Talep Edilen : ${controller.myJobs.data!.pendingJobsList![0].wORKTYPENAME != null ? "${controller.myJobs.data!.pendingJobsList![0].wORKTYPENAME}" : " "}",
-                  ),
-                  Text(
-                      "Durumu : ${controller.myJobs.data!.pendingJobsList![0].wORKSTATUSNAME}"),
-                ],
-              )
-            : const Center(child: CircularProgressIndicator())),
-      ),
-    );
+        body: Center(
+      child: Obx(() => controller.status.value.isSuccess
+          ? SizedBox(
+              height: 100.h,
+              width: 100.w,
+              child: ListView.builder(
+                itemCount:
+                    controller.myJobs.data!.pendingJobsList!.length, //değişecek
+                itemBuilder: (context, index) {
+                  return Container(
+                    height: 22.h,
+                    width: 100.w,
+                    margin:
+                        EdgeInsets.symmetric(horizontal: 2.w, vertical: 1.5.h),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(3.w),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                              blurRadius: 3,
+                              color: Colors.grey,
+                              spreadRadius: 0,
+                              offset: Offset(0, 1))
+                        ]),
+                    child: Padding(
+                      padding: EdgeInsets.all(.2.w),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                  height: 5.h,
+                                  width: 10.w,
+                                  margin: EdgeInsets.only(top: 1.h, left: 2.w),
+                                  child: Image.asset(
+                                    "assets/images/my_tasks/my_tasks_pic.JPG",
+                                    fit: BoxFit.cover,
+                                  )),
+                              SizedBox(
+                                width: 2.w,
+                              ),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding: EdgeInsets.only(bottom: 2.h),
+                                    child: Text(
+                                        //"18.06",
+                                        DateTimeConverTo.compareToDateTime(
+                                          controller
+                                              .myJobs
+                                              .data!
+                                              .pendingJobsList![index]
+                                              .wORKCREATIONDATE
+                                              .toString(),
+                                        ),
+                                        style: GoogleFonts.inter(
+                                            color: Const.ACIKLAMATEXTCOLOR,
+                                            fontSize: 1.4.h)),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 2.w, top: 1.h),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Column(
+                                  //talep no  column'ı
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Work Number',
+                                      style: GoogleFonts.inter(
+                                          fontSize: 1.7.h,
+                                          color: Const.ACIKLAMATEXTCOLOR,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    Padding(
+                                        padding: EdgeInsets.only(
+                                            top: 1.h, bottom: 1.h, left: 7.5.w),
+                                        child: controller
+                                                    .myJobs
+                                                    .data!
+                                                    .pendingJobsList![index]
+                                                    .iDWORK !=
+                                                null
+                                            ? Text(
+                                                controller
+                                                    .myJobs
+                                                    .data!
+                                                    .pendingJobsList![index]
+                                                    .iDWORK
+                                                    .toString(),
+                                                style: GoogleFonts.inter(
+                                                    color:
+                                                        Const.BASLIKTEXTCOLOR,
+                                                    fontWeight: FontWeight.w700,
+                                                    fontSize: 1.6.h),
+                                              )
+                                            : const Text("")),
+                                    Text(
+                                      'Requestor',
+                                      style: GoogleFonts.inter(
+                                          fontSize: 1.7.h,
+                                          color: Const.ACIKLAMATEXTCOLOR,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          vertical: 1.h, horizontal: .8.w),
+                                      child: FittedBox(
+                                        child: SizedBox(
+                                            height: 3.h,
+                                            width: 45.w,
+                                            child: controller
+                                                        .myJobs
+                                                        .data!
+                                                        .pendingJobsList![index]
+                                                        .eMPLOYEENAMESURNAME !=
+                                                    null
+                                                ? Text(
+                                                    controller
+                                                        .myJobs
+                                                        .data!
+                                                        .pendingJobsList![index]
+                                                        .eMPLOYEENAMESURNAME
+                                                        .toString(),
+                                                    overflow:
+                                                        TextOverflow.ellipsis,
+                                                    style: GoogleFonts.inter(
+                                                      color:
+                                                          Const.BASLIKTEXTCOLOR,
+                                                      fontWeight:
+                                                          FontWeight.w700,
+                                                      fontSize: 1.8.h,
+                                                    ),
+                                                  )
+                                                : const Text("")),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Column(
+                                  //açıklama  column'ı
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      'Status',
+                                      style: GoogleFonts.inter(
+                                          fontSize: 1.8.h,
+                                          color: Const.ACIKLAMATEXTCOLOR,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          vertical: 1.h, horizontal: .8.w),
+                                      child: FittedBox(
+                                        child: SizedBox(
+                                          height: 2.h,
+                                          width: 40.w,
+                                          child: Padding(
+                                            padding:
+                                                EdgeInsets.only(right: 5.w),
+                                            child: Text(
+                                              controller
+                                                  .myJobs
+                                                  .data!
+                                                  .pendingJobsList![index]
+                                                  .wORKSTATUSNAME
+                                                  .toString(),
+                                              overflow: TextOverflow.ellipsis,
+                                              style: GoogleFonts.inter(
+                                                  color: Const.BASLIKTEXTCOLOR,
+                                                  fontWeight: FontWeight.w700,
+                                                  fontSize: 1.7.h),
+                                            ),
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            )
+          : const CircularProgressIndicator()),
+    ));
   }
 }

17255d8b79385c1ee54d44b1efcaaaffcfbfb9c0
Onur Arda Karakum <onur.ardakarakum@outlook.com>
2023-01-16 14:33:19 +0300

    myApprovePage || Page's UI has been created. Info's next

--- lib/api/interceptors/request_interceptor.dart
+++ lib/api/interceptors/request_interceptor.dart
@@ -4,6 +4,6 @@ import 'package:get/get_connect/http/src/request/request.dart';
 
 FutureOr<Request> requestInterceptor(request) async {
   request.headers["vbtauthorization"] =
-      "AhYlfXdb2Z+bXJddyTbyCQwz3FumE+P78HQutg30i478WUZPnu1n90dH9sBk59mZ~1~string~638092931217494801";
+      "acuxhnoNavkmd9Glvq9l2uRJzIrq9O3euCrsG21YF7QNpcVcCef9Yt3B2/XPWs2E~1742~string~638094757867793947";
   return request;
 }

76efd2554027f2911d8332beae924aab7f50d0c3
Onur Arda Karakum <onur.ardakarakum@outlook.com>
2023-01-23 14:27:08 +0300

    develop || MyjobsDetail UI's revised

--- lib/app/modules/my_jobs_details_page/my_jobs_details_view.dart
+++ lib/app/modules/my_jobs_details_page/my_jobs_details_view.dart
@@ -5,6 +5,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:sun_ik_app/app/modules/my_jobs_details_page/my_jobs_details_controller.dart';
 
+import '../../../utils/base64_convert_picture.dart';
 import '../../../utils/date_convert.dart';
 
 class MyJobsDetailsView extends GetView<MyJobsDetailsController> {
@@ -41,50 +42,144 @@ class MyJobsDetailsView extends GetView<MyJobsDetailsController> {
                         image: AssetImage(
                             "assets/images/login/login_background.jpg"),
                         fit: BoxFit.cover)),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(top: 2.h),
-                      child: Text(
-                        "İş Oluşturulma Tarihi: ${DateTimeConverTo.compareToDateTime(controller.myJobsController.myJobs.data!.pendingJobsList![0].wORKCREATIONDATE.toString())}",
-                        style: GoogleFonts.inter(
-                            fontSize: 2.h, color: Colors.black),
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 5.w, vertical: 1.5.h),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(2.w),
+                            color: const Color(0xffF5F5F5),
+                            border:
+                                Border.all(width: .4.w, color: Colors.grey)),
+                        child: Padding(
+                          padding: EdgeInsets.all(2.w),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    "İş Oluşturulma Tarihi:",
+                                    style: GoogleFonts.inter(
+                                        fontSize: 2.h,
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.w600),
+                                  ),
+                                  Text(
+                                    "${DateTimeConverTo.compareToDateTime(controller.myJobsController.myJobs.data!.pendingJobsList![0].wORKCREATIONDATE.toString())}",
+                                    style: GoogleFonts.inter(
+                                        fontSize: 2.h, color: Colors.black),
+                                  ),
+                                ],
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 2.h),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Talep Eden:",
+                                      style: GoogleFonts.inter(
+                                          fontSize: 2.h,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Text(
+                                      controller
+                                                  .myJobsController
+                                                  .myJobs
+                                                  .data!
+                                                  .pendingJobsList![0]
+                                                  .eMPLOYEENAMESURNAME !=
+                                              null
+                                          ? "${controller.myJobsController.myJobs.data!.pendingJobsList![0].eMPLOYEENAMESURNAME}"
+                                          : "Belirtilmedi",
+                                      style: GoogleFonts.inter(
+                                          fontSize: 2.h, color: Colors.black),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 2.h),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Atanan Kişi:",
+                                      style: GoogleFonts.inter(
+                                          fontSize: 2.h,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Text(
+                                      controller
+                                                  .myJobsController
+                                                  .myJobs
+                                                  .data!
+                                                  .pendingJobsList![0]
+                                                  .aSSIGNEMPLOYEE !=
+                                              null
+                                          ? "${controller.myJobsController.myJobs.data!.pendingJobsList![0].aSSIGNEMPLOYEE}"
+                                          : "Belirtilmedi",
+                                      style: GoogleFonts.inter(
+                                          fontSize: 2.h, color: Colors.black),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(top: 2.h),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Durum:",
+                                      style: GoogleFonts.inter(
+                                          fontSize: 2.h,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                    Text(
+                                      controller
+                                                  .myJobsController
+                                                  .myJobs
+                                                  .data!
+                                                  .pendingJobsList![0]
+                                                  .wORKSTATUSNAME !=
+                                              null
+                                          ? "${controller.myJobsController.myJobs.data!.pendingJobsList![0].wORKSTATUSNAME}"
+                                          : "Belirtilmedi",
+                                      style: GoogleFonts.inter(
+                                          fontSize: 2.h, color: Colors.black),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 2.h),
-                      child: Text(
-                        "Talep Eden: ${controller.myJobsController.myJobs.data!.pendingJobsList![0].eMPLOYEENAMESURNAME}",
-                        style: GoogleFonts.inter(
-                            fontSize: 2.h, color: Colors.black),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 2.h),
-                      child: Text(
-                        "Atanan Kişi: ${controller.myJobsController.myJobs.data!.pendingJobsList![0].aSSIGNEMPLOYEE}",
-                        style: GoogleFonts.inter(
-                            fontSize: 2.h, color: Colors.black),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(top: 2.h),
-                      child: Text(
-                        "Durum: ${controller.myJobsController.myJobs.data!.pendingJobsList![0].wORKSTATUSNAME}",
-                        style: GoogleFonts.inter(
-                            fontSize: 2.h, color: Colors.black),
-                      ), // Status sabit pending değişecek ${}
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               )
             : SingleChildScrollView(
-                physics: const NeverScrollableScrollPhysics(),
+                physics: NeverScrollableScrollPhysics(),
                 child: Column(
                   children: [
-                    const LinearProgressIndicator(),
+                    LinearProgressIndicator(),
                     Container(
                       height: 90.h,
                       width: 100.w,
